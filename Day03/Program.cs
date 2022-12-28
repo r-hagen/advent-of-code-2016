c6aef86d16caf68890a1deb32c1ae6b31ba64534
Robert Hagen <hagen.robert@protonmail.com>
2022-12-28 19:25:11 +0100

    day 03: squares with three sides

---
+++ Day03/Program.cs
@@ -0,0 +1,13 @@
+﻿static bool NotEmpty(string s) => !string.IsNullOrWhiteSpace(s);
+var triangles = File.ReadLines("d03.in").Select(x => x.Split().Where(NotEmpty).Select(int.Parse).ToArray()).ToArray();
+
+static int IsValidTriangle(int a, int b, int c) => a + b > c && a + c > b && b + c > a ? 1 : 0;
+
+var possible1 = triangles.Sum(x => IsValidTriangle(x[0], x[1], x[2]));
+Console.WriteLine($"Part 1: {possible1}");
+
+var possible2 = triangles.Chunk(3).Sum(x =>
+    IsValidTriangle(x[0][0], x[1][0], x[2][0]) +
+    IsValidTriangle(x[0][1], x[1][1], x[2][1]) +
+    IsValidTriangle(x[0][2], x[1][2], x[2][2]));
+Console.WriteLine($"Part 2: {possible2}");
