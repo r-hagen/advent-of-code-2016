ba3cc0ca419e4d029710ceddcecda4c7ce361fd4
Robert Hagen <hagen.robert@protonmail.com>
2022-12-29 17:51:50 +0100

    day 06: signals and noise

---
+++ Day06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+var messages = File.ReadLines("d06.in").Select(x => x.ToArray()).ToArray();
+
+var sb = new StringBuilder();
+for (var col = 0; col < messages[0].Length; col++) {
+    var columnLetters = messages.Select(line => line[col]).ToArray();
+    var mostCommon = columnLetters.Distinct()
+        .Select(letter => (Letter: letter, Count: columnLetters.Count(x => x == letter)))
+        .MaxBy(t => t.Count)
+        .Letter;
+    sb.Append(mostCommon);
+}
+Console.WriteLine($"Part 1: {sb}");
+
+sb.Clear();
+for (var col = 0; col < messages[0].Length; col++) {
+    var columnLetters = messages.Select(line => line[col]).ToArray();
+    var leastCommon = columnLetters.Distinct()
+        .Select(letter => (Letter: letter, Count: columnLetters.Count(x => x == letter)))
+        .MinBy(t => t.Count)
+        .Letter;
+    sb.Append(leastCommon);
+}
+Console.WriteLine($"Part 2: {sb}");
